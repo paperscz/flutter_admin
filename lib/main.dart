a6f70ee9d6f3908214351ee27b44b8eab636cca4
wwmin <wwei.min@163.com>
2018-03-19 22:46:07 +0800

    word generate widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,65 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
+//    final wordPair = new WordPair.random();
     return new MaterialApp(
-        title: 'Welcome to Flutter',
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Welcome to Flutter'),
-          ),
-          body: new Center(
-//            child: new Text('Hello World'),
-              child: new Text(wordPair.asPascalCase)
-          ),
-        )
+      title: 'Welcome to Flutter',
+      home: new RandomWords(),
+//        home: new Scaffold(
+//          appBar: new AppBar(
+//            title: new Text('Welcome to Flutter'),
+//          ),
+//          body: new Center(
+////            child: new Text('Hello World'),
+////              child: new Text(wordPair.asPascalCase)
+//              child: new RandomWords()
+//          ),
+//        )
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+//    final wordPair = new WordPair.random();
+//    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generaor'),
+      ),
+      body: _buildSuggestions(),
     );
   }
 }
