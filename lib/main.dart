13aa9a844195eeb739a557365925817a5ed2dc6a
wwmin <wwei.min@163.com>
2018-03-19 23:43:54 +0800

    changes theme color

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
 //    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
+      theme: new ThemeData(
+          primaryColor: Colors.greenAccent
+      ),
       home: new RandomWords(),
 //        home: new Scaffold(
 //          appBar: new AppBar(
